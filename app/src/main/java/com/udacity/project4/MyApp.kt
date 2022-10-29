01c36cc80e5cf0bc67fd69866bfa0096f3243085
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-10-30 01:48:42 +0200

    added firebase authentication dependencies adjexted some dependencies in the app gradle file

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -35,6 +35,7 @@ class MyApp : Application() {
                     get() as ReminderDataSource
                 )
             }
+//
             single { RemindersLocalRepository(get()) as ReminderDataSource }
             single { LocalDB.createRemindersDao(this@MyApp) }
         }
--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,31 +1,32 @@
 package com.udacity.project4.locationreminders
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.navigation.fragment.NavHostFragment
-import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import androidx.navigation.findNavController
+import com.udacity.project4.databinding.ActivityRemindersBinding
+
+//import kotlinx.android.synthetic.main.activity_reminders.*
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    private lateinit var databinding: ActivityRemindersBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
+        databinding = ActivityRemindersBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_reminders)
+        setContentView(databinding.root)
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                databinding.navHostFragment.findNavController().popBackStack()
                 return true
             }
         }
